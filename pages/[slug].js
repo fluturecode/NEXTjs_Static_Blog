85b40a19bd05b4745cb0f7ac82bad3974ada90e8
Elizabeth Eidelson <fluturecode@gmail.com>
2020-10-08 14:21:21 -0400

    removed metadata from posts

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,8 +1,21 @@
 import React from "react";
 import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import Head from "next/head";
 
-const Post = ({ slug }) => {
-	return <div>The sluge for this page is: {slug}</div>;
+const Post = ({ contents, data }) => {
+	return (
+		<>
+			<Head>
+				<title>{data.title}</title>
+			</Head>
+			<div>
+				<div>Contents below</div>
+				<pre>{contents}</pre>
+			</div>
+		</>
+	);
 };
 
 // create paths
@@ -24,10 +37,17 @@ export const getStaticPaths = async () => {
 };
 
 // fetch data (get content of posts)
-export const getStaticProps = async ({ params: { slugh } }) => {
+export const getStaticProps = async ({ params: { slug } }) => {
+	const markdownWithMetadata = fs
+		.readFileSync(path.join("posts", slug + ".md"))
+		.toString();
+
+	const parsedMarkdown = matter(markdownWithMetadata);
+
 	return {
 		props: {
-			slug,
+			contents: parsedMarkdown.content,
+			data: parsedMarkdown.data,
 		},
 	};
 };
